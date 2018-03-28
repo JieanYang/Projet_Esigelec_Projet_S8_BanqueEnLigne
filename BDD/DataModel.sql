9e363a1cd0a8255584626bad1f6f8fb23468636f
JieanYang <yja8534786@gmail.com>
2018-03-28 15:39:25 +0200

    update of conception of BDD

--- BDD/DataModel.sql
+++ BDD/DataModel.sql
@@ -1,27 +1,24 @@
--- DROP
+SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `User`;
 DROP TABLE IF EXISTS `Compte`;
 DROP TABLE IF EXISTS `Transaction`;
 DROP TABLE IF EXISTS `Message`;
-DROP TABLE IF EXISTS `Cours de la bourse`;
+DROP TABLE IF EXISTS `Cours_de_la_bourse`;
 DROP TABLE IF EXISTS `Actualite`;
-DROP TABLE IF EXISTS `Services de la banque`;
-DROP DATABASE IF EXISTS `projet_s8_bank`;
-
-
--- CREATE
-
-CREATE DATABASE `projet_s8_bank` CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `projet_s8_bank`;
+DROP TABLE IF EXISTS `Services_de_la_banque`;
+SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE `User` (
     `id_user` INTEGER NOT NULL,
     `categorie_user` VARCHAR(15) NOT NULL,
     `nom` VARCHAR(15) NOT NULL,
     `prenon` VARCHAR(15) NOT NULL,
-    `numphone` INTEGER(30) NOT NULL,
     `email` VARCHAR(30) NOT NULL,
     `adresse` VARCHAR(50) NOT NULL,
+    `telephone` INTEGER(30) NOT NULL,
+    `ville` VARCHAR(15) NOT NULL,
+    `pays` VARCHAR(15) NOT NULL,
+    `dateNaissance` DATE NOT NULL,
     PRIMARY KEY (`id_user`)
 );
 
@@ -33,8 +30,7 @@ CREATE TABLE `Compte` (
     `solde` BIGINT NOT NULL,
     `date_create` DATE NOT NULL,
     `date_delete` DATE NOT NULL,
-    PRIMARY KEY (`id_compte`),
-    FOREIGN KEY (`id_user`) REFERENCES `User`(`id_user`)
+    PRIMARY KEY (`id_compte`)
 );
 
 CREATE TABLE `Transaction` (
@@ -42,12 +38,10 @@ CREATE TABLE `Transaction` (
     `categorie_transaction` VARCHAR(15) NOT NULL,
     `id_compte_emetteur` INTEGER NOT NULL,
     `id_compte_recepteur` INTEGER NOT NULL,
+    `date` DATE NOT NULL,
     `Somme` BIGINT NOT NULL,
     `description` TEXT NOT NULL,
-    `date` DATE NOT NULL,
-    PRIMARY KEY (`id_transaction`),
-    FOREIGN KEY (`id_compte_emetteur`) REFERENCES `Compte`(`id_compte`),
-    FOREIGN KEY (`id_compte_recepteur`) REFERENCES `Compte`(`id_compte`)
+    PRIMARY KEY (`id_transaction`)
 );
 
 CREATE TABLE `Message` (
@@ -62,7 +56,7 @@ CREATE TABLE `Message` (
     PRIMARY KEY (`id_message`)
 );
 
-CREATE TABLE `Cours de la bourse` (
+CREATE TABLE `Cours_de_la_bourse` (
     `id_entreprise` INTEGER NOT NULL,
     `entreprise` VARCHAR(15) NOT NULL,
     `prix` INTEGER NOT NULL,
@@ -72,13 +66,13 @@ CREATE TABLE `Cours de la bourse` (
 
 CREATE TABLE `Actualite` (
     `id_actualite` INTEGER NOT NULL,
+    `date` DATE NOT NULL,
     `title` TEXT NOT NULL,
     `text` TEXT NOT NULL,
-    `date` DATE NOT NULL,
     PRIMARY KEY (`id_actualite`)
 );
 
-CREATE TABLE `Services de la banque` (
+CREATE TABLE `Services_de_la_banque` (
     `id_service` INTEGER NOT NULL,
     `nom_service` VARCHAR(15) NOT NULL,
     `prix` INTEGER NOT NULL,
