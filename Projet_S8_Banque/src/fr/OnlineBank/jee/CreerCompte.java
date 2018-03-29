140df49723c72480f9a7cfdb0d62b1f0807467d2
JieanYang <yja8534786@gmail.com>
2018-03-29 17:13:24 +0200

    modification fichier java de CreerCompte

--- Projet_S8_Banque/src/fr/OnlineBank/jee/CreerCompte.java
+++ Projet_S8_Banque/src/fr/OnlineBank/jee/CreerCompte.java
@@ -2,6 +2,7 @@ package fr.OnlineBank.jee;
 
 import java.io.IOException;
 import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,12 +19,14 @@ public class CreerCompte extends HttpServlet {
 	
 	DAO monDAO= new DAO();
 	
-       
+//	  SimpleDateFormat sdfrmt = new SimpleDateFormat("dd-MM-yyyy");
+//	     sdfrmt.setLenient(false);
     /**
      * @see HttpServlet#HttpServlet()
      */
     public CreerCompte() {
         super();
+        
         // TODO Auto-generated constructor stub
     }
 
@@ -43,7 +46,13 @@ public class CreerCompte extends HttpServlet {
 		String nom = request.getParameter("nom");
 		String prenom = request.getParameter("prenom");
 		String email= request.getParameter("email");
-		String telephone= request.getParameter("telephone");
+		int telephone = 0;
+		try {
+			telephone = Integer.parseInt(request.getParameter("telephone"));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		String adresse = request.getParameter("adresse");
 		String pays = request.getParameter("pays");
 		String ville = request.getParameter("ville");
