b7ab5654972b30bd3ebf956c7aecaf80d44d29ae
Abdoul Mohamed <abdoul-karim.mohamed@groupe-esigelec.org>
2018-03-29 16:32:37 +0200

    maj index

--- Projet_S8_Banque/WebContent/css_script/scripts_index.js
+++ Projet_S8_Banque/WebContent/css_script/scripts_index.js
@@ -9,4 +9,13 @@ function myFunction() {
 	}
 }
 
-/* header */
+/* login form */
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
--- Projet_S8_Banque/src/fr/OnlineBank/jee/Login.java
+++ Projet_S8_Banque/src/fr/OnlineBank/jee/Login.java
@@ -13,29 +13,47 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	DAO aDAO = new DAO();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 
+		if (aDAO.login(username, password)) {
+			response.getWriter()
+					.write("<!DOCTYPE html>\r\n" + "<html>\r\n" + "	<head>\r\n" + "		<title>Page Title</title>\r\n"
+							+ "	</head>\r\n" + "	<body>\r\n" + "		<h1>You are logged in</h1>\r\n" + "	</body>\r\n"
+							+ "</html>");
+			// response.sendRedirect("HelloWorldMultiLanguage.jsp");
+		} else {
+			response.getWriter()
+					.write("<!DOCTYPE html>\r\n" + "<html>\r\n" + "	<head>\r\n" + "		<title>Page Title</title>\r\n"
+							+ "	</head>\r\n" + "	<body>\r\n" + "		<h1>FORBIDDEN</h1>\r\n" + "	</body>\r\n"
+							+ "</html>");
+		}
+	}
 }
